f56e78f4496479857d34c4d7c01164b66df65c7e
litwachi <katuka.wilfred8@gmail.com>
2018-10-23 16:00:39 +0300

    ft test endpoints for the CRUD endpoints

---
+++ tests/test_sign_in.py
@@ -0,0 +1,55 @@
+import json
+import unittest
+import os
+import psycopg2
+
+
+from app import create_app, DB_conns
+
+db = DB_conns()
+
+
+path = '/api/v1'
+
+
+class TestUsers(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.data = {
+            "username": "litwachi",
+            "password": "12345678"
+        }
+        self.data2 = {
+            "name": "Wilfred Katuka",
+            "username": "litwachi",
+            "password": "12345678",
+            "email": "katuka.wilfred8@gmail.com"
+        }
+
+    def test_user_signin(self):
+        """Test user sign in"""
+
+        # sign up a user
+        signup = self.client.post(path + '/auth/signup', data=json.dumps(self.data2), content_type='application/json')
+
+        # sign in an existing user
+        rs = self.client.post(path + 'auth/signin', data=json.dumps(self.data), content_type='application/json')
+        self.assertEqual(rs.status_code, 201)
+        rp = json.loads(rs.data)
+        self.assertEqual(rp["message"], "You have successfully logged in")
+
+        # validate user credentials
+        rs = self.client.post(path + '/auth/signin', data=json.dumps({
+            "username": "tester", "password": "12345678"}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 400)
+
+    def tests_access_token(self):
+        """Test for token generation upon signin"""
+        rs = self.client.post(path + '/auth/signin', data=json.dumps(self.data), content_type='application/json')
+        rp = json.loads(rs.data)
+        self.assertIn('User not yet registered', rp)
---
+++ tests/test_sign_up.py
@@ -0,0 +1,76 @@
+"""This class tests the user routes, signing"""
+import unittest
+import json
+import os
+import psycopg2
+
+from app import create_app, DB_conns
+
+db = DB_conns()
+
+path = '/api/v1'
+
+
+class TestUsers(unittest.TestCase):
+    """Test for users"""
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.data = {
+            "name": "Wilfred Katuka",
+            "username": "litwachi",
+            "password": "12345678",
+            "email": "katuka.wilfred8@gmail.com"
+        }
+
+    def test_user_signup(self):
+        """test post user register data"""
+
+        # existing user
+        results = self.client.post(path + 'auth/signup', data=json.dumps(self.data), content_type='application/json')
+        self.assertEqual(results.status_code, 400)
+        response = json.loads(results.data)
+        self.assertEqual(response["message"], "The user already exists")
+
+        # test username available
+        rs = self.client.post(path + 'auth/signup', data=json.dumps({
+            "name": "", "username": "litwachi", "password": "12345678", "email": "katuka.wilfred8@gmail.com"}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 400)
+
+        # test name validation
+        rs = self.client.post(path + '/auth/signup', data=json.dumps({
+            "name": "Wilfred Katuka", "username": " ", "password": "12345678", "email": "katuka.wilfred8@gmail.com"}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 400)
+
+        # test password
+        rs = self.client.post(path + '/auth/signup', data=json.dumps({
+            "name": "Wilfred Katuka", "username": "litwachi", "password": " ", "email": "katuka.wilfred8@gmail.com"}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 400)
+        # tests email validation
+        rs = self.client.post(path + '/auth/signup', data=json.dumps({
+            "name": "Wilfred Katuka", "username": "litwachi", "password": "12345678", "email": " "}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 400)
+
+        # user signup
+        rs = self.client.post(path + '/auth/signup', data=json.dumps({
+            "name": "tester", "username": "tester", "password": "12345678", "email": "tester@gmail.com"}),
+                              content_type='application/json')
+        self.assertEqual(rs.status_code, 201)
+        rp = json.loads(rs.data)
+        self.assertEqual(rp["message"], "Registration was successful")
+
+    def tearDown(self):
+
+        db.query("SELECT * FROM users WHERE username = %s", [self.data['username']])
+        user_data = db.cur.fetchone()
+        user_id = user_data[0]
+        db.query("DELETE from products WHERE user_id=%s", [user_id])
+        db.query("DELETE from users WHERE username=%s", [self.data['username']])
+
